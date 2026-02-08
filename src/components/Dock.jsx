60ddf9b80be2fa04280dafc1db43ae6f69bbad85
shikherpant <shikherpant@gmail.com>
2026-02-08 23:28:57 +0530

    updated icons path for deployment

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -2,6 +2,13 @@ import { useContext } from "react"
 import "./dock.scss"
 import { windowContext } from "../context/WindowControls"
 import githubIcon from "../assets/doc-icons/github.svg"
+import noteIcon from "../assets/doc-icons/note.svg"
+import pdfIcon from "../assets/doc-icons/pdf.svg"
+import calenderIcon from "../assets/doc-icons/calender.svg"
+import spotifyIcon from "../assets/doc-icons/spotify.svg"
+import mailIcon from "../assets/doc-icons/mail.svg"
+import linkIcon from "../assets/doc-icons/link.svg"
+import cliIcon from "../assets/doc-icons/cli.svg"
 
 const Dock = () => {
 
@@ -11,13 +18,13 @@ const Dock = () => {
   return (
     <section className="dock">
         <div className="icon github" onClick={()=>setWindowStates({...windowStates,github:true})}><img src={githubIcon} alt="" /></div>
-        <div className="icon note" onClick={()=>setWindowStates({...windowStates,notes:true})}><img src="/src/assets/doc-icons/note.svg" alt="" /></div>
-        <div className="icon pdf" onClick={()=>setWindowStates({...windowStates,pdf:true})}><img src="/src/assets/doc-icons/pdf.svg" alt="" /></div>
-        <div className="icon calender"><img src="/src/assets/doc-icons/calender.svg" alt="" onClick={()=>{window.open("https://calendar.google.com/calendar","_blank")}} /></div>
-        <div className="icon spotify" onClick={()=>setWindowStates({...windowStates,spotify:true})}><img src="/src/assets/doc-icons/spotify.svg" alt="" /></div>
-        <div className="icon mail"><img src="/src/assets/doc-icons/mail.svg" alt="" onClick={()=>{window.open("mailto:shikherpant@gmail.com")}} /></div>
-        <div className="icon link"><img src="/src/assets/doc-icons/link.svg" alt="" onClick={()=>{window.open("https://www.linkedin.com/in/shikher-pant-275b4019a/","_blank")}} /></div>
-        <div className="icon cli" onClick={()=>setWindowStates({...windowStates,terminal:true})}><img src="/src/assets/doc-icons/cli.svg" alt="" /></div>
+        <div className="icon note" onClick={()=>setWindowStates({...windowStates,notes:true})}><img src={noteIcon} alt="" /></div>
+        <div className="icon pdf" onClick={()=>setWindowStates({...windowStates,pdf:true})}><img src={pdfIcon} alt="" /></div>
+        <div className="icon calender"><img src={calenderIcon} alt="" onClick={()=>{window.open("https://calendar.google.com/calendar","_blank")}} /></div>
+        <div className="icon spotify" onClick={()=>setWindowStates({...windowStates,spotify:true})}><img src={spotifyIcon} alt="" /></div>
+        <div className="icon mail"><img src={mailIcon} alt="" onClick={()=>{window.open("mailto:shikherpant@gmail.com")}} /></div>
+        <div className="icon link"><img src={linkIcon} alt="" onClick={()=>{window.open("https://www.linkedin.com/in/shikher-pant-275b4019a/","_blank")}} /></div>
+        <div className="icon cli" onClick={()=>setWindowStates({...windowStates,terminal:true})}><img src={cliIcon} alt="" /></div>
     </section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import DateTime from "./DateTime"
 import "./nav.scss"
+import appleIcon from "../assets/navbar-icons/apple.svg"
+import wifiIcon from "../assets/navbar-icons/wifi.svg"
+
+
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="left">
-            <div className="icon"><img src="src/assets/navbar-icons/apple.svg" alt="" /></div>
+            <div className="icon"><img src={appleIcon} alt="" /></div>
             <div className="user">Shikher Pant</div>
             <div className="user">File</div>
             <div className="terminal">Terminal</div>
         </div>
         <div className="right">
-            <div className="wifi"><img src="src/assets/navbar-icons/wifi.svg" alt="" /></div>
+            <div className="wifi"><img src={wifiIcon} alt="" /></div>
             <div className="date-time">
                 <DateTime/>
             </div>
